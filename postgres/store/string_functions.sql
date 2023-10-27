88a2eebdd9b8af96fb34b54df27a650c3521671c
naferx <nafr.snabr@gmail.com>
2023-10-26 22:25:27 -0500

    new string functions

--- postgres/store/string_functions.sql
+++ postgres/store/string_functions.sql
@@ -12,3 +12,26 @@ SELECT
 	
 SELECT 
 	LENGTH('Data Science');
+
+
+SELECT 
+	SUBSTRING('partitioning', 2); -- artitioning
+
+SELECT 
+	SUBSTRING('partitioning' from 2); -- artitioning
+
+SELECT 
+	SUBSTRING('partitioning', 2, 4); -- arti
+	
+SELECT 
+	SUBSTRING('partitioning' from 2 for 4); -- arti
+
+-- similar to LEFT('partitioning', 4)
+SELECT
+	SUBSTRING('partitioning' for 4); -- part
+
+SELECT
+	FORMAT('Hello %s!', 'World');
+
+
+		
