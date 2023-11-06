a456ffd1f632720f5e2348f52b858648750fa242
naferx <nafr.snabr@gmail.com>
2023-11-06 08:17:32 -0500

    limit clause

---
+++ postgres/store/limit.sql
@@ -0,0 +1,24 @@
+
+-- The LIMIT clause limits the maximum number of records to return
+SELECT
+    id
+FROM
+    customers
+LIMIT 2;
+
+
+-- The OFFSET clause specifies where to start selecting the records to return
+SELECT
+    id
+FROM
+    orders; -- 1 2 3 4
+
+SELECT
+    id
+FROM
+    orders
+LIMIT 2 OFFSET 2; -- 3 4
+
+
+
+
