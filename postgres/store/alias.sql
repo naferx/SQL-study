49d9f4e141c3442ac27bf1be4f3e203e2f542dbb
naferx <nafr.snabr@gmail.com>
2024-07-19 19:12:45 -0500

    format examples of aliases

--- postgres/store/alias.sql
+++ postgres/store/alias.sql
@@ -1,72 +1,71 @@
-
 /**
-	Aliases are used to give a table or column a temporary name during the execution of a query. They're 
-	intended make column names more readable.
+	Aliases are used to give a table or column a temporary name during the execution of a query.
+	They're intended to make column names more readable
 	
 	Aliases are optional and are created using the AS keyword.
 	
 	The AS clause is used to relabel the output column
 **/
 
--- for naming columns using AS
-SELECT 
+-- for renaming output columns using AS
+SELECT
 	id AS identification
 FROM 
 	customers;
 
 -- not using AS since it's optional
-SELECT 
-	id identification
+SELECT
+	id ident
 FROM 
 	customers;
 
 
 -- not using AS since it's optional
-SELECT 
-	id ident, country
-FROM 
+SELECT
+	id identification, 
+    country
+FROM
 	customers;
 
 -- for naming tables
-SELECT 
+SELECT
 	c.id
-FROM 
+FROM
 	customers c;
 
 -- for naming tables using AS
-SELECT 
+SELECT
 	c.id
 FROM 
 	customers AS c;
 
-
--- Using the AS keyword and double quotes for creating alias that contains space characters
-SELECT 
+-- using the AS keyword and double quotes for creating alias with space characters
+SELECT
 	id AS "Id Number 1"  
 FROM 
 	customers;
 
--- Only double quotes for creating alias that contains space characters
-SELECT 
+-- only double quotes for creating alias with space characters
+SELECT
 	id "Id Number 2"  
 FROM 
 	customers;
 
--- The AS keyword is often used when two or more fields are concatenated into one.
--- Using double pipe (||) to concatenate columns 
-SELECT 
+-- the AS keyword is often used when two or more fields are concatenated into one.
+-- using double pipe (||) to concatenate columns 
+SELECT
 	id || first_name AS id_nombre 
 FROM 
 	customers;
 
 -- adding characters between the concatenated columns
-SELECT 
-	id || '-' || first_name AS id_nombre 
+SELECT
+    id || '-' || first_name AS id_nombre 
 FROM 
 	customers;
 
 -- adding characters between the concatenated columns using CONCAT function
-SELECT 
+SELECT
 	CONCAT(id, '-', first_name) AS id_nombre 
 FROM 
 	customers;
