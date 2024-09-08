d18d2e0e5b751c7c85bd6d25d76dc963c1aa2588
Nafer Sanabria <nafr.snabr@gmail.com>
2024-09-07 23:20:47 -0500

    document NULLIF function

--- postgres/store/conditional_functions.sql
+++ postgres/store/conditional_functions.sql
@@ -1,5 +1,22 @@
 /*
-    SQL-compliant conditional expressions available in PostgreSQL
-*/
+  SQL-compliant conditional expressions available in PostgreSQL
 
-SELECT COALESCE(NULL, 2) AS mycolumn;
+COALESCE(value [, ...])
+The COALESCE function returns the first of its arguments that is not null. Null is returned only if all
+arguments are null. It is often used to substitute a default value for null values when data is retrieved for
+display
+*/
+SELECT COALESCE(NULL) AS mycolumn; -- NULL
+SELECT COALESCE(NULL, 2) AS mycolumn; -- 2
+SELECT COALESCE(1, 2) AS mycolumn; -- 1
+SELECT COALESCE(NULL, NULL) AS mycolumn; -- NULL
+SELECT COALESCE(NULL, NULL, 3, 4) AS mycolumn; -- 3
+
+/*
+NULLIF(value1, value2)
+The NULLIF function returns a null value if value1 equals value2; otherwise it returns value1.
+This can be used to perform the inverse operation of the COALESCE
+*/
+SELECT NULLIF(1, 1) AS mycolumn; -- null
+SELECT NULLIF(1, 2) AS mycolumn; -- 1
+SELECT NULLIF(3, NULL) AS mycolumn; -- 3
